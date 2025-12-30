64e36327008602478e1ed4b566d9095c02f18a27
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2025-12-30 22:01:41 +0100

    Created the LoanType Enum

---
+++ src/BoligInfo.Core/Models/Loan.cs
@@ -0,0 +1,13 @@
+﻿using BoligInfo.Core.Enums;
+
+namespace BoligInfo.Core.Models;
+
+public class Loan
+{
+    public int Id { get; set; }
+    public LoanType LoanType { get; set; }
+    public double LoanAmount { get; set; }
+    public double InterestRate { get; set; }
+    public int LoanLifetime { get; set; }
+    public int EquityId { get; set; }
+}
