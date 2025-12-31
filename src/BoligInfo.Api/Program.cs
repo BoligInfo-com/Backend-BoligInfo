1a6e0911d0c634a0b26e223068b71da2246477a1
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2025-12-31 16:45:53 +0100

    Updated program.cs

--- src/BoligInfo.Api/Program.cs
+++ src/BoligInfo.Api/Program.cs
@@ -1,23 +1,37 @@
 using BoligInfo.Database;
-using BoligInfo.EquityRepository;
 using BoligInfo.LoanRepository;
+using BoligInfo.EquityRepository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add Authorization services
+builder.Services.AddAuthorization();
+
 // Add DbContext
 builder.Services.AddDbContext<BoligInfoDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException()));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Register repositories
 builder.Services.AddScoped<ILoanRepository, LoanRepository>();
 builder.Services.AddScoped<IEquityRepository, EquityRepository>();
 
-
 var app = builder.Build();
 
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseHttpsRedirection();
-app.UseAuthorization();
+app.UseAuthorization(); 
 app.MapControllers();
 
 app.Run();
