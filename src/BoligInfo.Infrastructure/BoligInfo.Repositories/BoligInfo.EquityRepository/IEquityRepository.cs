e1037797c7ebde09ac6712ff454e611a4c277529
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2025-12-31 16:13:17 +0100

    Created IEquityRepository

---
+++ src/BoligInfo.Infrastructure/BoligInfo.Repositories/BoligInfo.EquityRepository/IEquityRepository.cs
@@ -0,0 +1,14 @@
+﻿using BoligInfo.Core.Models;
+
+namespace BoligInfo.EquityRepository;
+
+public interface IEquityRepository
+{
+    Task<IEnumerable<Equity>> GetAllAsync();
+    Task<Equity?> GetByIdAsync(long id);
+    Task<Equity?> GetByIdWithLoansAsync(long id);
+    Task<Equity> AddAsync(Equity equity);
+    Task UpdateAsync(Equity equity);
+    Task DeleteAsync(long id);
+    Task<bool> ExistsAsync(long id);
+}
