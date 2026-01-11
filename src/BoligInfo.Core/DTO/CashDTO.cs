a17cf2ea7273240fa0f5b4814000e02298a30118
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2026-01-11 17:37:53 +0100

    Added Cash DTO's

---
+++ src/BoligInfo.Core/DTO/CashDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace BoligInfo.Core.DTO;
+
+public class CashDTO
+{
+    public long Id { get; init; }
+    public double CashAmount { get; set; }
+    public long EquityId { get; set; }
+}
+
+public class CreateCashDto
+{
+    public double CashAmount { get; set; }
+    public long EquityId { get; set; }
+}
+
+public class UpdateCashDto
+{
+    public double CashAmount { get; set; }
+}
