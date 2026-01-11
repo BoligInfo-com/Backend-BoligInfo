28e1cca7d81e95ca066677812a77fc493e635a61
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2026-01-11 15:42:14 +0100

    Seperated Loan configurations

--- src/BoligInfo.Infrastructure/BoligInfo.Database/BoligInfoDbContext.cs
+++ src/BoligInfo.Infrastructure/BoligInfo.Database/BoligInfoDbContext.cs
@@ -28,56 +28,8 @@ public class BoligInfoDbContext(DbContextOptions<BoligInfoDbContext> options) : 
         
         base.OnModelCreating(modelBuilder);
         
-        
-        // Configure Equity entity
+        // Configure entities
         new EquityEntityTypeConfiguration().Configure(modelBuilder.Entity<Equity>());
-        
-        // Configure Loan entity
-        modelBuilder.Entity<Loan>(entity =>
-        {
-            // Set table name
-            entity.ToTable("Loan");
-            
-            // Configure primary key
-            entity.HasKey(l => l.Id);
-            
-            // Map property names to database column names
-            entity.Property(l => l.Id)
-                .HasColumnName("ID")
-                .HasColumnType("bigint")
-                .ValueGeneratedOnAdd(); // Corresponds to nextval() in DB
-            
-            entity.Property(l => l.LoanType)
-                .HasColumnName("LoanType")
-                .IsRequired(false); // Not NULL is disabled in your schema
-            
-            entity.Property(l => l.LoanAmount)
-                .HasColumnName("LoanAmount")
-                .HasColumnType("double precision")
-                .HasDefaultValue(0)
-                .IsRequired(); // Not NULL is enabled
-            
-            entity.Property(l => l.InterestRate)
-                .HasColumnName("Rate")
-                .HasColumnType("double precision")
-                .IsRequired(); // Not NULL is enabled
-            
-            entity.Property(l => l.LoanLifetime)
-                .HasColumnName("LoanLifetime")
-                .HasColumnType("integer")
-                .IsRequired(false); // Not NULL is disabled
-            
-            entity.Property(l => l.EquityId)
-                .HasColumnName("EquityID")
-                .HasColumnType("bigint")
-                .IsRequired(); // Not NULL is enabled
-            
-            // Configure foreign key relationship
-            entity.HasOne<Equity>()
-                .WithMany(e => e.Loans)
-                .HasForeignKey(l => l.EquityId)
-                .HasConstraintName("Loan_EquityID_fkey")
-                .OnDelete(DeleteBehavior.Cascade);
-        });
+        new LoanEntityTypeConfiguration().Configure(modelBuilder.Entity<Loan>());
     }
 }
---
+++ src/BoligInfo.Infrastructure/BoligInfo.Database/LoanEntityTypeConfiguration.cs
@@ -0,0 +1,55 @@
+﻿using BoligInfo.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BoligInfo.Database;
+
+public class LoanEntityTypeConfiguration : IEntityTypeConfiguration<Loan>
+{
+    public void Configure(EntityTypeBuilder<Loan> builder)
+    {
+        builder.ToTable("Loan");
+        builder.HasKey(l => l.Id);
+        builder
+            .Property(l => l.Id)
+            .HasColumnName("ID")
+            .HasColumnType("bigint")
+            .ValueGeneratedOnAdd();
+        builder
+            .Property(l => l.LoanType)
+            .HasColumnName("LoanType")
+            .IsRequired(false);
+        builder
+            .Property(l => l.LoanAmount)
+            .HasColumnName("LoanAmount")
+            .HasColumnType("double precision")
+            .HasDefaultValue(0)
+            .IsRequired();
+        builder
+            .Property(l => l.InterestRate)
+            .HasColumnName("Rate")
+            .HasColumnType("double precision")
+            .IsRequired();
+        builder
+            .Property(l => l.LoanLifetime)
+            .HasColumnName("LoanLifetime")
+            .HasColumnName("LoanLifetime")
+            .IsRequired(false);
+        builder
+            .Property(l => l.EquityId)
+            .HasColumnName("EquityID")
+            .HasColumnType("bigint")
+            .IsRequired();
+        builder
+            .HasOne<Equity>()
+            .WithMany(e => e.Loans)
+            .HasForeignKey(l => l.EquityId)
+            .HasConstraintName("Loan_EquityID_fkey")
+            .OnDelete(DeleteBehavior.Cascade);
+        
+        builder.ToTable(t => t.HasCheckConstraint(
+                "CK_Loan_LoanAmount_NonNegative",
+                "\"LoanAmount\" >= 0"
+            ));
+    }
+}
