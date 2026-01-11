4b11d0ed0667998d166bbe3ab9b32e86b6289a5c
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2026-01-11 18:50:58 +0100

    Cash OnModelCreation Configuration

--- src/BoligInfo.Infrastructure/BoligInfo.Database/CashEntityTypeConfiguration.cs
+++ src/BoligInfo.Infrastructure/BoligInfo.Database/CashEntityTypeConfiguration.cs
@@ -8,6 +8,26 @@ public class CashEntityTypeConfiguration : IEntityTypeConfiguration<Cash>
 {
     public void Configure(EntityTypeBuilder<Cash> builder)
     {
-        
+        builder.ToTable("Cash");
+        builder.HasKey(c => c.Id);
+        builder
+            .Property(c => c.Id)
+            .HasColumnName("ID")
+            .HasColumnType("bigint")
+            .ValueGeneratedOnAdd();
+        builder
+            .Property(c => c.CashAmount)
+            .HasColumnName("CashAmount")
+            .HasColumnType("double precision")
+            .HasDefaultValue(0)
+            .IsRequired();
+
+        builder.ToTable(t =>
+        {
+            t.HasCheckConstraint(
+                "CK_Cash_CashAmount_NonNegative",
+                "\"CashAmount\" >= 0"
+            );
+        });
     }
 }
