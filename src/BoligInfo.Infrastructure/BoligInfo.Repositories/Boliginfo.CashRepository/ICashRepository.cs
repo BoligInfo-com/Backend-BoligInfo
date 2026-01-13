159a632b422caf46d5e88a3f91a06c3175a58e78
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2026-01-13 15:21:24 +0100

    Added CashRepository Interface

--- src/BoligInfo.Infrastructure/BoligInfo.Repositories/Boliginfo.CashRepository/ICashRepository.cs
+++ src/BoligInfo.Infrastructure/BoligInfo.Repositories/Boliginfo.CashRepository/ICashRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace Boliginfo.CashRepository;
+﻿using BoligInfo.Core.Models;
+
+namespace Boliginfo.CashRepository;
 
 public interface ICashRepository
 {
-    
+    Task<IEnumerable<Cash>> GetAllAsync();
+    Task<Cash?> GetByIdAsync(long id);
+    Task<IEnumerable<Cash>> GetByEquityIdAsync(long equityId);
+    Task<Cash> AddAsync(Cash cash);
+    Task UpdateAsync(Cash cash);
+    Task DeleteAsync(long id);
+    Task<bool> ExistsAsync(long id);
 }
