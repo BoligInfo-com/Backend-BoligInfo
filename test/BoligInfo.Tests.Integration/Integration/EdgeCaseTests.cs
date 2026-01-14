297055c9a2ff89e22bf35118171d41e087896d88
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2026-01-14 23:35:27 +0100

    Added EdgeCaseTests

---
+++ test/BoligInfo.Tests.Integration/Integration/EdgeCaseTests.cs
@@ -0,0 +1,56 @@
+﻿using BoligInfo.Database;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BoligInfo.Tests.Integration.Integration;
+
+// Base test class for setting up the test server
+public class IntegrationTestBase : IClassFixture<WebApplicationFactory<Program>>
+{
+    protected readonly HttpClient Client;
+    private readonly WebApplicationFactory<Program> _factory;
+
+    protected IntegrationTestBase(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<BoligInfoDbContext>));
+                if (descriptor != null)
+                    services.Remove(descriptor);
+
+                services.AddDbContext<BoligInfoDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase($"InMemoryTest_{Guid.NewGuid()}");
+                });
+
+                var sp = services.BuildServiceProvider();
+                using var scope = sp.CreateScope();
+                var db = scope.ServiceProvider.GetRequiredService<BoligInfoDbContext>();
+                db.Database.EnsureCreated();
+            });
+        });
+
+        Client = _factory.CreateClient();
+    }
+
+    protected async Task<BoligInfoDbContext> GetDbContextAsync()
+    {
+        var scope = _factory.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<BoligInfoDbContext>();
+        await context.Database.EnsureCreatedAsync();
+        return context;
+    }
+
+    protected async Task CleanDatabaseAsync()
+    {
+        var context = await GetDbContextAsync();
+        context.AllCash.RemoveRange(context.AllCash);
+        context.Loans.RemoveRange(context.Loans);
+        context.Equities.RemoveRange(context.Equities);
+        await context.SaveChangesAsync();
+    }
+}
