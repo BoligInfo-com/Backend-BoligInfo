99e4d463ec0767488345ba260b9ddfe0421e4681
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2026-01-14 22:12:34 +0100

    Added Controller endpoints for Cash

--- src/BoligInfo.Api/Controllers/CashController/AllCashController.cs
+++ src/BoligInfo.Api/Controllers/CashController/AllCashController.cs
@@ -1,4 +1,5 @@
 ﻿using BoligInfo.CashService;
+using BoligInfo.Core.DTO;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BoligInfo.Api.Controllers.CashController;
@@ -7,5 +8,55 @@ namespace BoligInfo.Api.Controllers.CashController;
 [Route("api/[controller]")]
 public class AllCashController(ICashService cashService) : ControllerBase, IAllCashController
 {
-    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<CashDto>>> GetAll()
+    {
+        var allCash = await cashService.GetAllCashAsync();
+        return Ok(allCash);
+    }
+
+    [HttpGet("{id:long}")]
+    public async Task<ActionResult<CashDto>> GetById(long id)
+    {
+        var cash = await cashService.GetCashByIdAsync(id);
+        if (cash == null)
+            return NotFound();
+        
+        return Ok(cash);
+    }
+
+    [HttpGet("equity/{equityId:long}")]
+    public async Task<ActionResult<IEnumerable<CashDto>>> GetByEquityId(long equityId)
+    {
+        var allCash = await cashService.GetAllCashByEquityIdAsync(equityId);
+        return Ok(allCash);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<CashDto>> Create(CreateCashDto createCashDto)
+    {
+        var cash = await cashService.CreateCashAsync(createCashDto);
+        return CreatedAtAction(nameof(GetById), new { id = cash.Id }, cash);
+    }
+
+    [HttpPut("{id:long}")]
+    public async Task<ActionResult<CashDto>> Update(long id, UpdateCashDto updateCashDto)
+    {
+        try
+        {
+            var cash = await cashService.UpdateCashAsync(id, updateCashDto);
+            return Ok(cash);
+        }
+        catch (KeyNotFoundException)
+        {
+            return NotFound();
+        }
+    }
+
+    [HttpDelete("{id:long}")]
+    public async Task<ActionResult> Delete(long id)
+    {
+        await cashService.DeleteCashAsync(id);
+        return NoContent();
+    }
 }
