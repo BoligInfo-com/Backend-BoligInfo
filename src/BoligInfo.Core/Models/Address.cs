88f9b0454e7108b631f81ca42345ebe64b0d1485
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2026-01-17 22:08:24 +0100

    Create Address.cs

---
+++ src/BoligInfo.Core/Models/Address.cs
@@ -0,0 +1,14 @@
+﻿namespace BoligInfo.Core.Models;
+
+public class Address
+{
+    public long Id  { get; set; }
+    public string Country { get; set; }
+    public string City { get; set; }
+    public string Zipcode { get; set; }
+    public string Street { get; set; }
+    public string? Number { get; set; }
+    public string? Suite {get; set;}
+    public int? Floor { get; set; }
+    public long HouseId { get; set; }
+}
