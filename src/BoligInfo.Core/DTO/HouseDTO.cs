6e0b5d88b08e2e10d0dfb37f01a88d09113e9522
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2026-01-17 19:23:46 +0100

    Added HouseDTO

---
+++ src/BoligInfo.Core/DTO/HouseDTO.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BoligInfo.Core.DTO;
+
+public class HouseDto
+{
+    public long Id { get; init; }
+    public double Price { get; set; }
+    public int? NumberOfRooms { get; set; }
+    public int? SquareMeters { get; set; }
+    public string? EnergyLabel { get; set; }
+    public DateOnly? PurchaseDate { get; set; }
+    public long EquityId { get; set; }
+    public ICollection<CashFlowDto>? CashFlows { get; set; }
+}
+
+public class CreateHouseDto
+{
+    [Required]
+    [Range(0, double.MaxValue, ErrorMessage = "Price must be zero or positive")]
+    public double Price { get; set; }
+    
+    [Range(0, int.MaxValue, ErrorMessage = "Number of rooms must be zero or positive")]
+    public int? NumberOfRooms { get; set; }
+    
+    [Range(0, int.MaxValue, ErrorMessage = "Square meters must be zero or positive")]
+    public int? SquareMeters { get; set; }
+    
+    public string? EnergyLabel { get; set; }
+    
+    public DateOnly? PurchaseDate { get; set; }
+    
+    [Required]
+    public long EquityId { get; set; }
+}
+
+public class UpdateHouseDto
+{
+    [Range(0, double.MaxValue, ErrorMessage = "Price must be zero or positive")]
+    public double? Price { get; set; }
+    
+    [Range(0, int.MaxValue, ErrorMessage = "Number of rooms must be zero or positive")]
+    public int? NumberOfRooms { get; set; }
+    
+    [Range(0, int.MaxValue, ErrorMessage = "Square meters must be zero or positive")]
+    public int? SquareMeters { get; set; }
+    
+    public string? EnergyLabel { get; set; }
+    
+    public DateOnly? PurchaseDate { get; set; }
+}
