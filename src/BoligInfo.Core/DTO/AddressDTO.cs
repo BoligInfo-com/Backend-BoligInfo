f6d5876ac6796aee657a5dc92c1cdec0b2177c54
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2026-01-17 22:34:58 +0100

    Added validation to DTOs

--- src/BoligInfo.Core/DTO/AddressDTO.cs
+++ src/BoligInfo.Core/DTO/AddressDTO.cs
@@ -1,4 +1,6 @@
-﻿namespace BoligInfo.Core.DTO;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BoligInfo.Core.DTO;
 
 public class AddressDto
 {
@@ -15,23 +17,52 @@ public class AddressDto
 
 public class CreateAddressDto
 {
+    [Required]
+    [MaxLength(180, ErrorMessage = "Country cannot exceed 180 characters")]
     public string Country { get; set; } = "Denmark";
+    
+    [Required]
+    [MaxLength(340, ErrorMessage = "Country cannot exceed 340 characters")]
     public string City { get; set; } = string.Empty;
+    
+    [Required]
+    [MaxLength(80, ErrorMessage = "Zipcode cannot exceed 80 characters")]
     public string Zipcode { get; set; } = string.Empty;
+    
+    [Required]
+    [MaxLength(340, ErrorMessage = "Street cannot exceed 340 characters")]
     public string Street { get; set; } = string.Empty;
+    
+    [MaxLength(20, ErrorMessage = "Number cannot exceed 20 characters")]
     public string? Number { get; set; }
+    
+    [MaxLength(20, ErrorMessage = "Suite cannot exceed 20 characters")]
     public string? Suite {get; set;}
+    
     public int? Floor { get; set; }
+    
+    [Required]
     public long HouseId { get; set; }
 }
 
 public class UpdateAddressDto
 {
+    [MaxLength(180, ErrorMessage = "Country cannot exceed 180 characters")]
     public string? Country { get; set; }
+    
+    [MaxLength(340, ErrorMessage = "Country cannot exceed 340 characters")]
     public string? City { get; set; } 
+    
+    [MaxLength(80, ErrorMessage = "Zipcode cannot exceed 80 characters")]
     public string? Zipcode { get; set; } 
+    
+    [MaxLength(340, ErrorMessage = "Street cannot exceed 340 characters")]
     public string? Street { get; set; }
+    
+    [MaxLength(20, ErrorMessage = "Number cannot exceed 20 characters")]
     public string? Number { get; set; }
+    
+    [MaxLength(20, ErrorMessage = "Suite cannot exceed 20 characters")]
     public string? Suite {get; set;}
     public int? Floor { get; set; }
 }
---
+++ src/BoligInfo.Infrastructure/BoligInfo.Database/AddressEntityTypeConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using BoligInfo.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BoligInfo.Database;
+
+public class AddressEntityTypeConfiguration : IEntityTypeConfiguration<Address>
+{
+    public void Configure(EntityTypeBuilder<Address> builder)
+    {
+        builder.ToTable("Address");
+        builder.HasKey(a => a.Id);
+        
+        builder
+            .Property(a => a.Id)
+            .HasColumnName("ID")
+            .HasColumnType("bigint")
+            .ValueGeneratedOnAdd();
+        
+        builder
+            .Property(a => a.Country)
+            .HasColumnName("Country")
+            .HasColumnType("character varying(180)")
+            .HasMaxLength(180)
+            .IsRequired();
+    }
+}
