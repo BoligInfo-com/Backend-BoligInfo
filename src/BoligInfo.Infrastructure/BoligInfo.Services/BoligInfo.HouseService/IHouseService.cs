910ff5144c125f85338b2f40d4da8ceef5bfe284
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2026-01-17 19:38:32 +0100

    Added interface for HouseService

---
+++ src/BoligInfo.Infrastructure/BoligInfo.Services/BoligInfo.HouseService/IHouseService.cs
@@ -0,0 +1,14 @@
+﻿using BoligInfo.Core.DTO;
+
+namespace BoligInfo.HouseService;
+
+public interface IHouseService
+{
+    Task<IEnumerable<HouseDto>> GetAllHousesAsync();
+    Task<HouseDto?> GetHouseByIdAsync(long id);
+    Task<IEnumerable<HouseDto>> GetHousesByEquityIdAsync(long equityId);
+    Task<HouseDto?> GetHouseWithCashFlowsAsync(long id);
+    Task<HouseDto> CreateHouseAsync(CreateHouseDto createHouseDto);
+    Task<HouseDto> UpdateHouseAsync(long id, UpdateHouseDto updateHouseDto);
+    Task DeleteHouseAsync(long id);
+}
