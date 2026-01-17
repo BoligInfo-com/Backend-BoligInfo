70d4130088a08c3bdc371b99d47abfc5c6e70c3c
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2026-01-17 22:23:13 +0100

    Create AddressDTO.cs

---
+++ src/BoligInfo.Core/DTO/AddressDTO.cs
@@ -0,0 +1,37 @@
+﻿namespace BoligInfo.Core.DTO;
+
+public class AddressDto
+{
+    public long Id  { get; set; }
+    public string Country { get; set; } = "Denmark";
+    public string City { get; set; } = string.Empty;
+    public string Zipcode { get; set; } = string.Empty;
+    public string Street { get; set; } = string.Empty;
+    public string? Number { get; set; }
+    public string? Suite {get; set;}
+    public int? Floor { get; set; }
+    public long HouseId { get; set; }
+}
+
+public class CreateAddressDto
+{
+    public string Country { get; set; } = "Denmark";
+    public string City { get; set; } = string.Empty;
+    public string Zipcode { get; set; } = string.Empty;
+    public string Street { get; set; } = string.Empty;
+    public string? Number { get; set; }
+    public string? Suite {get; set;}
+    public int? Floor { get; set; }
+    public long HouseId { get; set; }
+}
+
+public class UpdateAddressDto
+{
+    public string? Country { get; set; }
+    public string? City { get; set; } 
+    public string? Zipcode { get; set; } 
+    public string? Street { get; set; }
+    public string? Number { get; set; }
+    public string? Suite {get; set;}
+    public int? Floor { get; set; }
+}
