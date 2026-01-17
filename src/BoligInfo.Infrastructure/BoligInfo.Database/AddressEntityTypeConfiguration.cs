268e103a7c6db1b36436cf3f94a013e11805c147
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2026-01-17 23:01:10 +0100

    EFCore Configuration for Address

--- src/BoligInfo.Infrastructure/BoligInfo.Database/AddressEntityTypeConfiguration.cs
+++ src/BoligInfo.Infrastructure/BoligInfo.Database/AddressEntityTypeConfiguration.cs
@@ -21,7 +21,61 @@ public class AddressEntityTypeConfiguration : IEntityTypeConfiguration<Address>
             .Property(a => a.Country)
             .HasColumnName("Country")
             .HasColumnType("character varying(180)")
+            .HasDefaultValue("Denmark")
             .HasMaxLength(180)
+            .IsRequired();
+        
+        builder
+            .Property(a => a.City)
+            .HasColumnName("City")
+            .HasColumnType("character varying(340)")
+            .HasMaxLength(340)
+            .IsRequired();
+        
+        builder
+            .Property(a => a.Zipcode)
+            .HasColumnName("Zipcode")
+            .HasColumnType("character varying(80)")
+            .HasMaxLength(80)
+            .IsRequired();
+        
+        builder
+            .Property(a => a.Street)
+            .HasColumnName("Street")
+            .HasColumnType("character varying(34o)")
+            .HasMaxLength(340)
+            .IsRequired();
+        
+        builder
+            .Property(a => a.Number)
+            .HasColumnName("Number")
+            .HasColumnType("character varying(20)")
+            .HasMaxLength(20)
+            .IsRequired(false);
+        
+        builder
+            .Property(a => a.Suite)
+            .HasColumnName("Suite")
+            .HasColumnType("character varying(20)")
+            .HasMaxLength(20)
+            .IsRequired(false);
+        
+        builder
+            .Property(a => a.Floor)
+            .HasColumnName("Floor")
+            .HasColumnType("integer")
+            .IsRequired(false);
+        
+        builder
+            .Property(a => a.HouseId)
+            .HasColumnName("HouseId")
+            .HasColumnType("bigint")
+            .IsRequired();
+        
+        builder
+            .HasOne<House>()
+            .WithOne(h => h.Address)
+            .HasForeignKey<Address>(a => a.HouseId)
             .IsRequired();
     }
 }
