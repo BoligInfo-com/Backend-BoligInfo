f4057265d9749e7dad072aa321c753bd2d12b34b
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2026-01-18 22:35:50 +0100

    Added AddressesController interface

---
+++ src/BoligInfo.Api/Controllers/AddressController/IAddressesController.cs
@@ -0,0 +1,14 @@
+﻿using BoligInfo.Core.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoligInfo.Api.Controllers.AddressController;
+
+public interface IAddressesController
+{
+    Task<ActionResult<IEnumerable<AddressDto>>> GetAll();
+    Task<ActionResult<AddressDto>> GetById(long id);
+    Task<ActionResult<AddressDto>> GetByHouseId(long houseId);
+    Task<ActionResult<AddressDto>> Create(CreateAddressDto createAddressDto);
+    Task<ActionResult<AddressDto>> Update(long id, UpdateAddressDto updateAddressDto);
+    Task<ActionResult> Delete(long id);
+}
