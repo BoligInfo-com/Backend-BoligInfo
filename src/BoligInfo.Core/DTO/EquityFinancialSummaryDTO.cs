04019ed0de7ebfe1321330efe99e1afaa955a2f0
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2026-01-20 19:18:35 +0100

    Create EquityFinancialSummaryDTO.cs

---
+++ src/BoligInfo.Core/DTO/EquityFinancialSummaryDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace BoligInfo.Core.DTO;
+
+public class EquityFinancialSummaryDto
+{
+    public long EquityId { get; set; }
+    public int NumberOfHouses { get; set; }
+    public decimal TotalMonthlyIncome { get; set; }
+    public decimal TotalMonthlyExpenses { get; set; }
+    public decimal NetMonthlyCashFlow { get; set; }
+}
