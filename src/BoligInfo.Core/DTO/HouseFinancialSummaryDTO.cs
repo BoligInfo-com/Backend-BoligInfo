8a46f998af53e1e20f27ade8ce4aaa57f71a77b5
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2026-01-20 19:18:11 +0100

    Create HouseFinancialSummaryDTO.cs

---
+++ src/BoligInfo.Core/DTO/HouseFinancialSummaryDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BoligInfo.Core.DTO;
+
+public class HouseFinancialSummaryDto
+{
+    public long HouseId { get; set; }
+    public decimal MonthlyIncome { get; set; }
+    public decimal MonthlyExpenses { get; set; }
+    public decimal NetMonthlyCashFlow { get; set; }
+}
