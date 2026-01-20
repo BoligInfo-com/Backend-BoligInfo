8cc26c4f7d390b58783f79e4c2699da803ef8d56
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2026-01-20 19:19:17 +0100

    Create PortfolioSummaryDTO.cs

---
+++ src/BoligInfo.Core/DTO/PortfolioSummaryDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace BoligInfo.Core.DTO;
+
+public class PortfolioSummaryDto
+{
+    public decimal TotalMonthlyCashFlow { get; set; }
+    public decimal TotalAnnualCashFlow { get; set; }
+    public int TotalProperties { get; set; }
+}
