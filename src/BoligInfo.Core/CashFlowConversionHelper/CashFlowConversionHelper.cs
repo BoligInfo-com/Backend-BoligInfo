82600a2043477099d463c3fea58fef216088a231
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2026-01-20 19:16:32 +0100

    Create CashFlowConversionHelper.cs

---
+++ src/BoligInfo.Core/CashFlowConversionHelper/CashFlowConversionHelper.cs
@@ -0,0 +1,19 @@
+﻿using BoligInfo.Core.Enums;
+using BoligInfo.Core.Models;
+
+namespace BoligInfo.Core.CashFlowConversionHelper;
+
+public class CashFlowConversionHelper
+{
+    private static double ToMonthlyAmount(CashFlow cf)
+    {
+        return cf.Frequency switch
+        { 
+            Frequency.MONTHLY => cf.Amount,
+            Frequency.QUARTERLY => cf.Amount / 3,
+            Frequency.YEARLY => cf.Amount / 12,
+            Frequency.ONE_TIME => 0,
+            _ => 0
+        };
+    }
+}
