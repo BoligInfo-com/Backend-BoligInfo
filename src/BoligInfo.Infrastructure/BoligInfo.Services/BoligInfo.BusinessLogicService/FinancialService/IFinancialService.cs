fec611bb3bbd7f7d992cf817cf9b516c1a50a7a3
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2026-01-20 19:21:34 +0100

    Create FinancialService interface

---
+++ src/BoligInfo.Infrastructure/BoligInfo.Services/BoligInfo.BusinessLogicService/FinancialService/IFinancialService.cs
@@ -0,0 +1,9 @@
+﻿using BoligInfo.Core.DTO;
+
+namespace BoligInfo.BusinessLogic.FinancialService;
+
+public interface IFinancialInsightService
+{
+    Task<HouseFinancialSummaryDto> GetHouseSummaryAsync(long houseId);
+    Task<EquityFinancialSummaryDto> GetEquitySummaryAsync(long equityId);
+}
