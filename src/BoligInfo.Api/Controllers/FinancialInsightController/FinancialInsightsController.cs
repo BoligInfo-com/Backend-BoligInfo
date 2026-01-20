fab7f97e167aa17d8d38ba0df37a36783239a30e
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2026-01-20 19:38:46 +0100

    Added FinancialInsightsController

---
+++ src/BoligInfo.Api/Controllers/FinancialInsightController/FinancialInsightsController.cs
@@ -0,0 +1,38 @@
+﻿using BoligInfo.BusinessLogic.FinancialService;
+using BoligInfo.Core.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoligInfo.Api.Controllers.FinancialInsightController;
+
+[ApiController]
+[Route("api/financial-insights")]
+public class FinancialInsightsController(
+    IFinancialInsightService service
+) : ControllerBase
+{
+    [HttpGet("house/{houseId:long}")]
+    public async Task<ActionResult<HouseFinancialSummaryDto>> GetHouseSummary(long houseId)
+    {
+        try
+        {
+            return Ok(await service.GetHouseSummaryAsync(houseId));
+        }
+        catch (KeyNotFoundException)
+        {
+            return NotFound();
+        }
+    }
+
+    [HttpGet("equity/{equityId:long}")]
+    public async Task<ActionResult<EquityFinancialSummaryDto>> GetEquitySummary(long equityId)
+    {
+        try
+        {
+            return Ok(await service.GetEquitySummaryAsync(equityId));
+        }
+        catch (KeyNotFoundException)
+        {
+            return NotFound();
+        }
+    }
+}
---
+++ src/BoligInfo.Api/Controllers/FinancialInsightController/IFinancialInsightsController.cs
@@ -0,0 +1,10 @@
+﻿using BoligInfo.Core.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoligInfo.Api.Controllers.FinancialInsightController;
+
+public interface IFinancialInsightsController
+{
+    public Task<ActionResult<HouseFinancialSummaryDto>> GetHouseSummary(long houseId);
+    public Task<ActionResult<EquityFinancialSummaryDto>> GetEquitySummary(long equityId);
+}
